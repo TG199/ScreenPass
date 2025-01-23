c8ab8b343e926a3ee5ec54d4c0ba208e2527a622
TG <ebiritg@gmail.com>
2025-01-24 00:08:15 +0100

    isAuthenticated middleware

---
+++ src/middleware/isAuthenticated.js
@@ -0,0 +1,7 @@
+const isAuthenticated = (req, res, next) => {
+    if (req.user) {
+        return next()
+    }
+    res.status(403).json({msg: 'Access denied. Log in first!'})
+    return res.redirect('/login')
+}
