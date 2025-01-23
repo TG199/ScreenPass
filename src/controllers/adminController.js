e5dd46396dc56fec9b1e72d9357317b704ad1ffd
TG <ebiritg@gmail.com>
2025-01-23 23:54:41 +0100

    admin controller: user and movie operations

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -81,23 +81,118 @@ class AdminController {
 
     // Movie operations
     static async createMovie(req, res) {
-           
+        const { 
+        title,
+        description,
+        posterImage,
+        genre,
+        rating,
+        releaseDate,
+        duration
+          } = req.body;
+        try {
+            const movie = new Movie({
+                title,
+                description,
+                posterImage,
+                genre,
+                rating,
+                releaseDate,
+                duration
+            });
+            await movie.save();
+            res.status(201).json(movie);
+        } catch (err) {
+            console.error("Error creating movie:", err);
+            res.status(500).json({msg: "Server error", err: err.message});
+        }
     }
 
     static async updateMovie(req, res) {
+        const { title, description, posterImage, genre, rating, releaseDate, duration } = req.body;
 
+        try {
+            const movie = await Movie.findById(req.params.id);
+            if (!movie) {
+                return res.status(404).json({msg: "Movie not found"});
+            }
+            movie.title = title;
+            movie.description = description;
+            movie.posterImage = posterImage;
+            movie.genre = genre;
+            movie.rating = rating;
+            movie.releaseDate = releaseDate;
+            movie.duration = duration;
+
+            await movie.save();
+            res.status(200).json(movie);
+        } catch (err) {
+            console.error("Error updating movie:", err);
+            res.status(500).json({msg: "Server error", err: err.message});
+        }
     }
 
     static async deleteMovie(req, res) {
-
+        const { id } = req.params;
+        try {
+            const movie = await Movie.findById(id);
+            if (!movie) {
+                return res.status(404).json({msg: "Movie not found"});
+            }
+            await movie.remove();
+        } catch (err) {
+            console.error("Error deleting movie:", err);
+            res.status(500).json({msg: "Server error", err: err.message});
+        }
     }
+
     static async getAllMovies(req, res) {
+        try {
+            const movies = await Movie.find();
+            res.status(200).json(movies);
+        } catch (err) {
+            console.error("Error fetching movies:", err)
+            res.status(500).json({msg: "Server error", err: err.message});
+        }
+    }
+
+
+    // Showtime operations
+    static async createShowtime(req, res) {
 
     }
 
+    static async updateShowtime(req, res) {
+
+    }
+
+    static async deleteShowtime(req, res) {
+
+    }
+
+    static async getAllShowtimes(req, res) {
+
+    }
+
+    static async getShowtime(req, res) {
+
+    }
+    // Reservation operations}
+    static async getReservation(req, res) {
+
+    }
+
+    static async deleteReservation(req, res) {
+
+    }
+
+    static async updateReservation(req, res) {
+    
+    }
     static async getAllReservations(req, res) {
 
     }
+
 }
 
 module.exports = AdminController;
