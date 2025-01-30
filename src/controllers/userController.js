29c16b8a88ce53cdede41bc2a4da1b977457f7b4
TG <ebiritg@gmail.com>
2025-01-30 14:22:04 +0100

    Modify user controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,178 +1,119 @@
-const User = require('../models/User')
-const Reservation = require('../models/Reservation')
-const Movie = require('../models/Movie')
+const User = require('../models/User');
+const Reservation = require('../models/Reservation');
+const Movie = require('../models/Movie');
 const Showtime = require('../models/Showtime');
 const asyncHandler = require('../utils/AsyncHandler');
 const CustomError = require('../utils/CustomError');
-//const isAuthenticated = require('../middleware/isAuthenticated')
-
+const mongoose = require('mongoose');
 
 class UserController {
-
-    static reserveSit = asyncHandler(async (req, res) => {
-        const { id, showtimeId, movieId } = req.params
-
-        const user = await User.findById(id);
-
-        if (!user) {
-            throw new CustomError("User not found", 400);
-        }
-
+    
+    static reserveSeat = asyncHandler(async (req, res) => {
+        const { id, showtimeId, movieId } = req.params;
         const { seatNumber } = req.body;
 
         if (!seatNumber) {
             throw new CustomError("Seat number is required", 400);
         }
 
-        const movieExists = await Movie.findById(movieId)
+        const session = await mongoose.startSession();
+        session.startTransaction();
 
-        if (!movieExists)
-        {
-            throw new CustomError("Can't reserve sit, selected movie is not availble", 404)
+        try {
+            const user = await User.findById(id).session(session);
+            if (!user) throw new CustomError("User not found", 404);
+
+            const movie = await Movie.findById(movieId).session(session);
+            if (!movie) throw new CustomError("Selected movie is not available", 404);
+
+            const showtime = await Showtime.findOne({ _id: showtimeId, movie: movieId }).session(session);
+            if (!showtime) throw new CustomError("No showtime allocated for this movie", 404);
+
+            const seat = showtime.availableSeats.find(seat => seat.seatNumber === seatNumber);
+            if (!seat || seat.isReserved) {
+                throw new CustomError("Seat is not available, choose another one", 400);
+            }
+
+            seat.isReserved = true;
+            await showtime.save({ session });
+
+            const reservation = await Reservation.create([{ 
+                user: user._id, 
+                movie: movie._id, 
+                showtime: showtime._id, 
+                status: "reserved", 
+                date: showtime.date, 
+                seatNumber
+            }], { session });
+
+            await session.commitTransaction();
+            session.endSession();
+
+            res.status(201).json(reservation[0]);
+        } catch (error) {
+            await session.abortTransaction();
+            session.endSession();
+            throw error;
         }
-        const movieShowtime = await Showtime.findOne({_id: showtimeId,
-            movie: movieId
-        })
-
-        if (!movieShowtime){
-            throw new CustomError("No showtime allocated for movie", 404);
-        }
-
-        const seat = movieShowtime.availableSeats.find(seat => seat.seatNumber === seatNumber)
-
-        if (!seat) {
-            throw new CustomError("Seat is not available, try another one", 400);
-        }
-        seat.isReserved = true;
-
-        await movieShowtime.save();
-
-        const status = "reserved";
-        const reservation = new Reservation({
-            "user": user._id,
-            "movie": movieExists._id,
-            "showtime": movieShowtime._id,
-            "status": status,
-            "date": movieShowtime.date,
-            "seatNumber": seatNumber
-        })
-
-        await reservation.save()
-        res.status(200).json(reservation)
-
-
-        
     });
 
     static getMoviesAndShowtime = asyncHandler(async (req, res) => {
         const movies = await Movie.find().populate("showtimes");
-
-        if (!movies.length) {
-            throw new CustomError("No movies found", 404);
-        }
-
+        if (!movies.length) throw new CustomError("No movies found", 404);
         res.status(200).json(movies);
-    
     });
 
     static availableSeats = asyncHandler(async (req, res) => {
-        const { showtimeId } = req.params
+        const { showtimeId } = req.params;
 
-        const user = req.user
-
-        if (!user) {
-            throw new CustomError("Unathorized", 401);
-        }
-
-        const matchingShowtime = await Showtime.findById(showtimeId, {
-            "availableSeats": {$elemMatch: { isReserved: false} }
+        const showtime = await Showtime.findById(showtimeId, { 
+            availableSeats: { $elemMatch: { isReserved: false } } 
         });
 
-        if (!matchingShowtime) {
-            throw new CustomError("No matching showtime found", 404)
-        }
+        if (!showtime) throw new CustomError("No matching showtime found", 404);
 
-        const  available = matchingShowtime.availableSeats.filter(seat => seat.isReserved === false);
+        const available = showtime.availableSeats.map(seat => seat.seatNumber);
+        if (!available.length) throw new CustomError("No available seats at this time", 400);
 
-        if (available.length === 0) {
-            throw new CustomError("There are no available seats at this time");
-        }
-
-        res.status(200).json({availableSeats: available.map(seat => seat.seatNumber)});
+        res.status(200).json({ availableSeats: available });
     });
 
     static viewReservation = asyncHandler(async (req, res) => {
-        const { userId, reservationId }  = req.params
+        const { userId, reservationId } = req.params;
 
-        const user = req.user.id;
-        if (user != userId) {
-            throw new CustomError("Unathorized, user is not authorized", 403);
-        }
-        const userReservation = await User.findById(userId, {
-            "reservations": {$elemMatch: { _id: reservationId}}
-        });
+        if (req.user.id !== userId) throw new CustomError("Unauthorized access", 403);
 
-        if (!userReservation) {
-            throw new CustomError("No reservation found for this user", 404);
-        }
+        const reservation = await Reservation.findOne({ _id: reservationId, user: userId })
+            .populate("movie showtime");
+        if (!reservation) throw new CustomError("Reservation not found", 404);
 
-        res.status(200).json({
-            reservation: userReservation
-        });
+        res.status(200).json({ reservation });
     });
 
     static cancelReservation = asyncHandler(async (req, res) => {
-        const { userId, reservationId} = req.params;
+        const { userId, reservationId } = req.params;
+        if (req.user.id !== userId) throw new CustomError("Unauthorized access", 403);
 
-        const user = req.user.id;
+        const reservation = await Reservation.findOne({ _id: reservationId, user: userId })
+            .populate("showtime");
 
-        if (user != userId) {
-            throw new CustomError("Unathorized, user is not authorized", 403);
+        if (!reservation) throw new CustomError("No reservation found", 404);
+
+        if (new Date(reservation.date) <= new Date()) {
+            throw new CustomError("Cannot cancel a past showtime reservation", 400);
         }
 
-         
-        const reservation = await User.findOne(
-            {_id: userId},
-            {reservations: {$elemMatch: { _id: reservationId}}
-        }).populate({
-            path:  "reservations.showtime",
-            select: "availableSeats date"
-        })
-
-        if (!reservation || ! reservation.reservations.length) {
-            throw new CustomError("No reservation found", 404);
-        }
-        const currentReservation = reservation.reservations[0];
-        const  showtime = currentReservation.showtime;
-
-        const upcoming = new Date(currentReservation.date) > new Date();
-
-        if (!upcoming) {
-            throw new CustomError("You can't cancel reservation for past showtime", 400);
+        const showtime = reservation.showtime;
+        const seat = showtime.availableSeats.find(seat => seat.seatNumber === reservation.seatNumber);
+        if (seat) {
+            seat.isReserved = false;
+            await showtime.save();
         }
 
         await Reservation.findByIdAndDelete(reservationId);
 
-        const seat = showtime.availableSeats.find(seat => seat.seatNumber === currentReservation.seatNumber);
-
-        if (seat) {
-            seat.isReserved = false;
-
-            await showtime.save();
-        } else {
-            throw new CustomError("Seat not found in Showtime available seats", 404);
-        }
-
-        res.status(200).json({
-            message: "Reservation canceled",
-            canceledReservation: {
-                reservationId,
-                movie: currentReservation.movie,
-                showtime: currentReservation.showtime,
-                seatNumber: currentReservation.seatNumber,
-                date: currentReservation.date
-            }
-        });
-    })
-
+        res.status(200).json({ message: "Reservation canceled successfully", canceledReservation: reservation });
+    });
 }
+
+module.exports = UserController;
